b382a36e92abc383e6ecfc4a2d9c5b4d0abd34b6
alfian_ezza <rianezza86@gmail.com>
2022-04-05 11:41:25 +0700

    Update counseling, idk

--- speakup/src/assets/pages/Counseling.jsx
+++ speakup/src/assets/pages/Counseling.jsx
@@ -1,12 +1,50 @@
 import React from "react";
 import HeroImage from "../../assets/images/counseling-hero.svg";
 import icon_check from "../../assets/images/icon_check.svg";
-
+import CounselingItem from "../../items/CounselingItem";
 import Header from "../../components/Header";
 import Faq from "../../components/Faq";
 import Footer from "../../components/Footer";
 
-function Counseling(props) {
+class Counseling extends React.Component{
+
+    state = {
+        users: [],
+    }
+
+    getCounselingAPI = () => {
+        fetch("https://speakupapi.herokuapp.com/api/users?role.id=2")
+          .then((response) => response.json())
+          .then((CounselingAPI) => {
+            this.setState({
+              kelompok: CounselingAPI,
+            });
+          })
+          .catch((error) => {
+            console.log(error);
+          });
+      };
+
+    // getBenefitAPI = () => {
+    //     fetch("https://speakupapi.herokuapp.com/api/course-type?id=1")
+    //     .then((response) => response.json())
+    //     .then((BenefitAPI) => {
+    //         this.setState({
+    //         kelompok: BenefitAPI,
+    //         });
+    //     })
+    //     .catch((error) => {
+    //         console.log(error);
+    //     });
+    // };
+
+    // Sampai difix, uncomment code ini engga bakal munculin apapun
+    // componentDidMount() {
+    //     this.getCounselingApi();
+    //     // this.getBenefitAPI();
+    // }
+    
+    render() {
     return (
         <div>
             <Header />
@@ -28,75 +66,15 @@ function Counseling(props) {
                     </div>
                 </div>
                 {/* POPULAR COURSE */}
-                <div className="row popular-counseling mt-5">
-
+                <div className="row popular-counseling md-5">
                     <h1>Popular Counseling</h1>
-
-                    <div className="row">
-                        <div className="col-md-4 col-sm-6">
-                            <div className="card custom-card-course">
-                                <img src="https://images.unsplash.com/photo-1580489944761-15a19d654956?ixlib=rb-1.2.1&ixid=MnwxMjA3fDB8MHxzZWFyY2h8N3x8YXZhdGFyfGVufDB8fDB8fA%3D%3D&auto=format&fit=crop&w=600&q=60" className="card-img-top conselor-image" alt="..." />
-                                <div className="card-body conseling-body mt-3">
-                                    <div className="row">
-                                        <div className="col-md">
-                                            <h5 className="card-title conselor-name">Edward James</h5>
-                                            <h6 className="conselor-job">Psikolog at Zahair Foundation</h6>
-                                        </div>
-                                        <div className="col-md-4 text-end">
-                                            <p className="text-primary text-via">Via: Zoom</p>
-                                        </div>
-                                    </div>
-                                    <hr className="my-3" />
-                                    {/* BENEFIT */}
-                                    <div className="row px-2">
-                                        <div className="col-md-6 mb-2">
-                                            <div className="row d-flex align-items-center">
-                                                <img src={icon_check} className="icon_check align-self-center" alt="" />
-                                                <h5 className="benefit-item my-auto">Catatan Konsultasi</h5>
-                                            </div>
-                                        </div>
-                                        <div className="col-md-6 mb-2">
-                                            <div className="row d-flex align-items-center">
-                                                <img src={icon_check} className="icon_check align-self-center" alt="" />
-                                                <h5 className="benefit-item my-auto">Tes Kesehatan Mental</h5>
-                                            </div>
-                                        </div>
-                                        <div className="col-md-6 mb-2">
-                                            <div className="row d-flex align-items-center">
-                                                <img src={icon_check} className="icon_check align-self-center" alt="" />
-                                                <h5 className="benefit-item my-auto">Worksheet</h5>
-                                            </div>
-                                        </div>
-                                        <div className="col-md-6 mb-2">
-                                            <div className="row d-flex align-items-center">
-                                                <img src={icon_check} className="icon_check align-self-center" alt="" />
-                                                <h5 className="benefit-item my-auto">Tes Kepribadian</h5>
-                                            </div>
-                                        </div>
-                                        <div className="col-md-6 mb-2">
-                                            <div className="row d-flex align-items-center">
-                                                <img src={icon_check} className="icon_check align-self-center" alt="" />
-                                                <h5 className="benefit-item my-auto">Lembar hasil psikotes</h5>
-                                            </div>
-                                        </div>
-                                        <div className="col-md-6 mb-2">
-                                            <div className="row d-flex align-items-center">
-                                                <img src={icon_check} className="icon_check align-self-center" alt="" />
-                                                <h5 className="benefit-item my-auto">Tes minat karier</h5>
-                                            </div>
-                                        </div>
-                                    </div>
-                                    <hr className="my-3" />
-                                    <div className="row d-flex align-items-center ps-2 mb-2">
-                                        <h3 className="my-auto p-0 mx-0 counseling-price">IDR 450.000</h3>
-                                        <div className="col-md d-flex justify-content-end">
-                                            <button className="btn btn-primary btn-small">Enroll Now</button>
-                                        </div>
-                                    </div>
-                                </div>
-                            </div>
-                        </div>
-                        <div className="col-md-4 col-sm-6">
+                <div className="row">
+                    {[1,2].map((item) => {
+                        return <Counseling key={item.id}, name={item.}
+                        })
+                    }
+                    {/* I think this is the card item? */}
+                        {/* <div className="col-md-4 col-sm-6">
                             <div className="card custom-card-course">
                                 <img src="https://images.unsplash.com/photo-1607746882042-944635dfe10e?ixlib=rb-1.2.1&ixid=MnwxMjA3fDB8MHxzZWFyY2h8OXx8YXZhdGFyfGVufDB8fDB8fA%3D%3D&auto=format&fit=crop&w=600&q=60" className="card-img-top conselor-image" alt="..." />
                                 <div className="card-body conseling-body mt-3">
@@ -108,10 +86,10 @@ function Counseling(props) {
                                         <div className="col-md-4 text-end">
                                             <p className="text-primary text-via">Via: Zoom</p>
                                         </div>
-                                    </div>
-                                    <hr className="my-3" />
+                                    </div> */}
+                                    {/* <hr className="my-3" /> */}
                                     {/* BENEFIT */}
-                                    <div className="row px-2">
+                                    {/* <div className="row px-2">
                                         <div className="col-md-6 mb-2">
                                             <div className="row d-flex align-items-center">
                                                 <img src={icon_check} className="icon_check align-self-center" alt="" />
@@ -158,7 +136,7 @@ function Counseling(props) {
                                     </div>
                                 </div>
                             </div>
-                        </div>
+                        </div> */}
                         <div className="col-md-4 col-sm-6">
                             <div className="card custom-card-course">
                                 <img src="https://images.unsplash.com/photo-1438761681033-6461ffad8d80?ixlib=rb-1.2.1&ixid=MnwxMjA3fDB8MHxzZWFyY2h8MTB8fGF2YXRhcnxlbnwwfHwwfHw%3D&auto=format&fit=crop&w=600&q=60" className="card-img-top conselor-image" alt="..." />
@@ -224,10 +202,8 @@ function Counseling(props) {
                         </div>
                     </div>
                 </div>
-
-                <div className="row popular-counseling mt-5">
+                <div className="row popular-counseling md-5">
                     <h1>Other Counseling</h1>
-
                     <div className="row">
                         <div className="col-md-4 col-sm-6">
                             <div className="card custom-card-course">
@@ -421,10 +397,10 @@ function Counseling(props) {
                     </div>
                 </div>
             </div>
-            <Faq />
-            <Footer />
-        </div>
+            <Faq/>
+            <Footer/>
+            </div>
     );
+    }
 }
-
 export default Counseling;
---
+++ speakup/src/items/CounselingItem.jsx
@@ -0,0 +1,71 @@
+import React from "react";
+import icon_check from "../assets/images/icon_check.svg";
+
+const CounselingItem = (props) => {
+    return(
+        <div className="col-md-4 col-sm-6">
+                        <div className="card custom-card-course">
+                            <img src="https://images.unsplash.com/photo-1580489944761-15a19d654956?ixlib=rb-1.2.1&ixid=MnwxMjA3fDB8MHxzZWFyY2h8N3x8YXZhdGFyfGVufDB8fDB8fA%3D%3D&auto=format&fit=crop&w=600&q=60" className="card-img-top conselor-image" alt="..." />
+                            <div className="card-body conseling-body mt-3">
+                                <div className="row">
+                                    <div className="col-md">
+                                        <h5 className="card-title conselor-name">{props.name}</h5>
+                                        <h6 className="conselor-job">{props.job}</h6>
+                                    </div>
+                                    <div className="col-md-4 text-end">
+                                        <p className="text-primary text-via">Via: Zoom</p>
+                                    </div>
+                                </div>
+                                <hr className="my-3" />
+                                <div className="row px-2">
+                                    <div className="col-md-6 mb-2">
+                                        <div className="row d-flex align-items-center">
+                                            <img src={icon_check} className="icon_check align-self-center" alt="" />
+                                            <h5 className="benefit-item my-auto">Catatan Konsultasi</h5>
+                                        </div>
+                                    </div>
+                                    <div className="col-md-6 mb-2">
+                                        <div className="row d-flex align-items-center">
+                                            <img src={icon_check} className="icon_check align-self-center" alt="" />
+                                            <h5 className="benefit-item my-auto">Tes Kesehatan Mental</h5>
+                                        </div>
+                                    </div>
+                                    <div className="col-md-6 mb-2">
+                                        <div className="row d-flex align-items-center">
+                                            <img src={icon_check} className="icon_check align-self-center" alt="" />
+                                            <h5 className="benefit-item my-auto">Worksheet</h5>
+                                        </div>
+                                    </div>
+                                    <div className="col-md-6 mb-2">
+                                        <div className="row d-flex align-items-center">
+                                            <img src={icon_check} className="icon_check align-self-center" alt="" />
+                                            <h5 className="benefit-item my-auto">Tes Kepribadian</h5>
+                                        </div>
+                                    </div>
+                                    <div className="col-md-6 mb-2">
+                                        <div className="row d-flex align-items-center">
+                                            <img src={icon_check} className="icon_check align-self-center" alt="" />
+                                            <h5 className="benefit-item my-auto">Lembar hasil psikotes</h5>
+                                        </div>
+                                    </div>
+                                    <div className="col-md-6 mb-2">
+                                        <div className="row d-flex align-items-center">
+                                            <img src={icon_check} className="icon_check align-self-center" alt="" />
+                                            <h5 className="benefit-item my-auto">Tes minat karier</h5>
+                                        </div>
+                                    </div>
+                                </div>
+                                <hr className="my-3" />
+                                <div className="row d-flex align-items-center ps-2 mb-2">
+                                    <h3 className="my-auto p-0 mx-0 counseling-price">IDR 450.000</h3>
+                                    <div className="col-md d-flex justify-content-end">
+                                        <button className="btn btn-primary btn-small">Enroll Now</button>
+                                    </div>
+                                </div>
+                            </div>
+                        </div>
+                    </div>
+    );
+}
+
+export default CounselingItem;
