3f2ec7c3b9007ab3be38707cf2fd6411b77adccc
MOH IBNU ABDURROHMAN SUTIO <ibnuabdurrohmansutio@gmail.com>
2022-04-07 04:42:58 +0700

    add button category diary
    Co-Authored-By: E41200368_Nila Amalia_Gol A <74764135+nilamalia02@users.noreply.github.com>
    Co-Authored-By: Tharixs Akbar <70683226+Tharixs@users.noreply.github.com>
    Co-Authored-By: NOGA MUKTIWATI <80508180+NOGAMUKTIWATI@users.noreply.github.com>
    Co-Authored-By: Alfian Ezza Raditya <rianezza86@gmail.com>

--- speakup/src/assets/pages/Diaries.jsx
+++ speakup/src/assets/pages/Diaries.jsx
@@ -9,7 +9,6 @@ import axios from "axios";
 import { useState } from "react";
 
 const Diaries = (props) => {
-
     // get all data from diaries api
     const [diaries, setDiaries] = React.useState([]);
     useEffect(() => {
@@ -17,6 +16,15 @@ const Diaries = (props) => {
             .then(res => {
                 setDiaries(res.data);
             })
+    });
+
+    // get all date diary type
+    const [diaryTypes, setDiaryType] = useState([]);
+    useEffect(() => {
+        axios.get('https://speakupapi.herokuapp.com/api/diary-types')
+            .then(res => {
+                setDiaryType(res.data);
+            });
     });
 
     // get search data
@@ -33,27 +41,26 @@ const Diaries = (props) => {
                     <form className="col-md-6 mt-3">
                         <input type="text" placeholder="Search" className="form-control" onChange={(event) => {
                             setSearch(event.target.value);
-                        }}/>
+                        }} />
                     </form>
                 </div>
 
-                <div className="row justify-content-center category-button">
-                    <div className="col-md text-center">
-                        <button className="btn btn-primary">Productivity</button>
-                    </div>
-                    <div className="col-md text-center">
-                        <button className="btn btn-default">Relationship</button>
-                    </div>
-                    <div className="col-md text-center">
-                        <button className="btn btn-default">Mental Health</button>
-                    </div>
-                    <div className="col-md text-center">
-                        <button className="btn btn-default">Life Plan</button>
-                    </div>
-                    <div className="col-md text-center">
-                        <button className="btn btn-default">Other</button>
-                    </div>
-                </div>
+                <center>
+                    <ul className="nav nav-pills mb-5 flex-column flex-sm-row" id="pills-tab" role="tablist">
+                        {
+                            diaryTypes.map((diaryType, index) => {
+                                return (
+                                    <li className="nav-item flex-sm-fill" role="presentation" key={diaryType.id}>
+                                        <button className="nav-link btn" id={diaryType.id} data-bs-toggle="pill" data-bs-target="#pills-tips" type="button" role="tab" aria-controls="pills-tips" aria-selected="false">{diaryType.name}</button>
+                                    </li>
+                                );
+                            })
+                        }
+                        <li className="nav-item flex-sm-fill" role="presentation">
+                            <button className="nav-link btn" id="pills-tips-tab" data-bs-toggle="pill" data-bs-target="#pills-tips" type="button" role="tab" aria-controls="pills-tips" aria-selected="false">All</button>
+                        </li>
+                    </ul>
+                </center>
 
                 <div className="row list-diary">
                     <h1>List Of Diary</h1>
@@ -61,11 +68,11 @@ const Diaries = (props) => {
                     <div className="row diary-items">
                         {
                             diaries?.filter((val) => {
-                                if(searchTerm == "") {
+                                if (searchTerm == "") {
                                     return val;
-                                } else if(val.title.toLowerCase().includes(searchTerm.toLowerCase())) {
+                                } else if (val.title.toLowerCase().includes(searchTerm.toLowerCase())) {
                                     return val;
-                                } else if(val.detail_user.name.toLowerCase().includes(searchTerm.toLowerCase())) {
+                                } else if (val.detail_user.name.toLowerCase().includes(searchTerm.toLowerCase())){
                                     return val;
                                 }
                             }).map((diary) => {
